8d3540acf000b8822b8cdf3485a468e778336b9d
Tonatiuh Flores Castelán <tona.castelan16@gmail.com>
2019-12-11 01:06:36 -0600

    Apply fixes from StyleCI (#2)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -11,13 +11,14 @@ class JokeFactory
 
     public function __construct(Client $client = null)
     {
-        $this->client = $client ? : new Client();
+        $this->client = $client ?: new Client();
     }
 
     public function getRandomJoke()
     {
         $response = $this->client->get(self::API_ENDPOINT);
         $joke = json_decode($response->getBody()->getContents());
+
         return $joke->value->joke;
     }
 }
